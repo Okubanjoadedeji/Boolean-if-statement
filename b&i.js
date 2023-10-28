da116f2e3b9b97699ca9ec13bcdb155dc1991aa1
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-10-28 21:02:36 +0100

    statements

---
+++ b&i.js
@@ -0,0 +1,37 @@
+true
+false
+
+//const age = 16;
+
+//.log(3 > 5);
+//.log(5 === 5);
+/*if (false) {
+ console.log('good morning');
+} else {
+ console.log('good night');}
+
+if (age >= 18) {
+  console.log('You are an adult');
+} else if (age >= 16) {
+  console.log('Almost there');
+} else {
+console.log('You have to be atleast 18 to view this content');
+}*/
+
+/*console.log(false || true);
+console.log(false && true);
+console.log(!true); */
+
+
+const result= 0 ? 'truthy' : 'falsy';
+console.log(result);
+
+
+
+false && console.log('hello');
+
+const message = 5 && 'hello';
+console.log(message)
+
+
+
